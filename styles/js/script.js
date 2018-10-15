813be5727eae8b9c4b7e0cd4451a63232b9d111d
aleksandar1302 <salejankovic95@gmail.com>
2018-10-16 00:53:02 +0200

    Update on js(onclick function),crop pics, font update, logo

---
+++ styles/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function (){
+    $("#oNama").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#onama").offset().top
+        }, 2000);
+    });
+});
+$(document).ready(function (){
+    $("#Kontakt").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#kontakt").offset().top
+        }, 2000);
+    });
+});
