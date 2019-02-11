5d2e990055a0a7d1aafe6baa2f6ee51973a141f3
aleksandar1302 <salejankovic95@gmail.com>
2019-02-11 19:01:42 +0100

    Main image slider updated

--- styles/js/script.js
+++ styles/js/script.js
@@ -28,8 +28,30 @@ $(".nav li").on("click", function() {
     $(".nav li").removeClass("active");
     $(this).addClass("active");
   });
-$(document).ready(function() {
-$('.image').cycle({
-	fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
-	});
-});
+
+  $next = 1;			// fixed, please do not modfy;
+  $current = 0;		// fixed, please do not modfy;
+  $interval = 3000;	// You can set single picture show time;
+  $fadeTime = 1000;	// You can set fadeing-transition time;
+  $imgNum = 2;		// How many pictures do you have
+
+  $(document).ready(function(){
+      //NOTE : Div Wrapper should with css: relative;
+      //NOTE : img should with css: absolute;
+      //NOTE : img Width & Height can change by you;
+      $('.image').css('position','relative');
+    //   $('.image img').css({'position':'absolute','width':'332px','height':'500px'});
+
+      nextFadeIn();
+  });
+
+  function nextFadeIn(){
+      //make image fade in and fade out at one time, without splash vsual;
+      $('.image img').eq($current).delay($interval).fadeOut($fadeTime)
+      .end().eq($next).delay($interval).hide().fadeIn($fadeTime, nextFadeIn);
+          
+      // if You have 5 images, then (eq) range is 0~4 
+      // so we should reset to 0 when value > 4; 
+      if($next < $imgNum-1){ $next++; } else { $next = 0;}
+      if($current < $imgNum-1){ $current++; } else { $current =0; }
+  };
