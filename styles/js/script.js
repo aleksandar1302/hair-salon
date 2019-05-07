59218dd84caef3c7ae773a2a1b4d473a1631059c
aleksandar1302 <salejankovic95@gmail.com>
2019-05-07 15:30:13 +0200

    added contact form, styled, links to zakazi.html, deleted part of js script for #kontakt on index.html and #kontakt deleted on index.html

--- styles/js/script.js
+++ styles/js/script.js
@@ -20,13 +20,6 @@ $(document).ready(function (){
         }, 2000);
     });
 });
-$(document).ready(function (){
-    $("#Kontakt").click(function (){
-        $('html, body').animate({
-            scrollTop: $("#kontakt").offset().top
-        }, 2000);
-    });
-});
 $('.navbar-nav li a' ).on("click", function(){
     $('#hamburger-menu').click();
 });
